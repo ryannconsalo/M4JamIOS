2533ae48ef35aedb62fcf5dbdaf0fd1c1aeb70e1
Emily Koagedal <elkoagedal@gmail.com>
2017-07-03 11:44:26 +0200

    Added location tracking

--- M4Jam/ViewController.swift
+++ M4Jam/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import WebKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var webView: UIWebView!
     
@@ -18,6 +21,26 @@ class ViewController: UIViewController {
         
         let myURL = URL(string: "https://app.m4jam.com/app/client/jobbers/sign-up/#login")
         webView.loadRequest(URLRequest(url: myURL!))
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.distanceFilter = 10000
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //get most recent coordinate
+        let myCoor = locations[0]
+        //get lat and lon
+        let myLat = myCoor.coordinate.latitude
+        let myLon = myCoor.coordinate.longitude
+        let myCoor2D = CLLocationCoordinate2D(latitude: myLat, longitude: myLon)
+        
+        print("location changed")
     }
     
     override func didReceiveMemoryWarning() {
